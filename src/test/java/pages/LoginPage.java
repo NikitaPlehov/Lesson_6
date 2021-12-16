25790f588b55424b2342b0168eb9bab688a3e961
Nikita <bojiodia1997@mail.ru>
2021-12-16 08:30:16 +0300

    Lesson8 доделал урок!

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -1,11 +1,12 @@
 package pages;
 
+import baseEntities.BasePage;
 import com.tms.core.ReadProperties;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class LoginPage {
+public class LoginPage extends BasePage {
     // Описание элементов страницы
     private By PAGE_OPENED_IDENTIFIER = By.className("logo-loginpage");
 
