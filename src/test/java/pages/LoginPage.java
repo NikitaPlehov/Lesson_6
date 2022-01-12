92acd6833e9acf3bfbd0e1cf2381e1c673e08f69
Nikita <bojiodia1997@mail.ru>
2022-01-12 20:57:05 +0300

    Lesson 12_1!

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -4,15 +4,21 @@ import baseEntities.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
-import static com.sun.xml.internal.ws.api.ComponentFeature.Target.ENDPOINT;
 
 public class LoginPage extends BasePage {
+    private static String ENDPOINT = "/auth/login";
     protected By PAGE_OPENED_IDENTIFIER = By.id("button_primary");
 
-    protected By emailSelector = By.id("name");
-    protected By passwordSelector = By.id("password");
-    protected By loginSelector = By.id("button_primary");
+    @FindBy(id = "name")
+    public WebElement emailField;
+
+    @FindBy(id = "password")
+    public WebElement passwordField;
+
+    @FindBy(id = "button_primary")
+    public WebElement loginField;
 
     public LoginPage(WebDriver driver) {
         super(driver);
@@ -27,15 +33,4 @@ public class LoginPage extends BasePage {
         return !super.isPageOpened(PAGE_OPENED_IDENTIFIER);
     }
 
-    public WebElement getEmailField() {
-        return driver.findElement(emailSelector);
-    }
-
-    public WebElement getPasswordField() {
-        return driver.findElement(passwordSelector);
-    }
-
-    public WebElement getLoginButton() {
-        return driver.findElement(loginSelector);
-    }
 }
--- src/test/java/tests/SmokeTest.java
+++ src/test/java/tests/SmokeTest.java
@@ -14,9 +14,9 @@ public class SmokeTest extends BaseTest {
     public void loginTest() {
         LoginPage loginPage = new LoginPage(driver);
 
-        loginPage.getEmailField().sendKeys(ReadProperties.getUsername());
-        loginPage.getPasswordField().sendKeys(ReadProperties.getPassword());
-        loginPage.getLoginButton().click();
+        loginPage.emailField.sendKeys(ReadProperties.getUsername());
+        loginPage.passwordField.sendKeys(ReadProperties.getPassword());
+        loginPage.loginField.click();
 
         DashboardPage dashboardPage = new DashboardPage(driver);
         waits.waitForVisibility(dashboardPage.getAddProjectButton());
@@ -28,9 +28,9 @@ public class SmokeTest extends BaseTest {
     public void flakyLoginTest() {
         LoginPage loginPage = new LoginPage(driver);
 
-        loginPage.getEmailField().sendKeys(ReadProperties.getUsername());
-        loginPage.getPasswordField().sendKeys(ReadProperties.getPassword());
-        loginPage.getLoginButton().click();
+        loginPage.emailField.sendKeys(ReadProperties.getUsername());
+        loginPage.passwordField.sendKeys(ReadProperties.getPassword());
+        loginPage.loginField.click();
 
         DashboardPage dashboardPage = new DashboardPage(driver);
         waits.waitForVisibility(dashboardPage.getAddProjectButton());
