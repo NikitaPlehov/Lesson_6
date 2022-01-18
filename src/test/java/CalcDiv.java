983af43150915f1f91526cad120b3b82ef0ff156
Nikita <bojiodia1997@mail.ru>
2022-01-18 15:21:37 +0300

    TestNG_NM готовое

--- src/test/java/CalcDiv.java
+++ src/test/java/CalcDiv.java
@@ -13,13 +13,26 @@ public class CalcDiv {
 
     @Test
     public void testDiv1() {
-        Assert.assertEquals(calc.div(9, 0), 9);
+        Assert.assertEquals(calc.div(9, 1), 9);
     }
 
     @Test (timeOut = 1000, expectedExceptions = ThreadTimeoutException.class)
     public void testDiv2() throws InterruptedException {
         Assert.assertEquals(calc.div(15, 5), 3);
         Thread.sleep(2000);
+    }
+
+    @Test
+    public void divTest1() {
+        Assert.assertEquals(calc.div(6, 2), 3);
+    }
+
+    public int a = 36;
+    public int b = 10;
+
+    @Test(expectedExceptions = ArithmeticException.class)
+    public void divTest2() {
+        System.out.println("a / b = " + (a / 0));
     }
 }
 
