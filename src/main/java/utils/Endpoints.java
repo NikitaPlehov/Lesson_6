86c3275260a1a6702f5710eea5ad9951c1fb378a
Nikita <bojiodia1997@mail.ru>
2022-01-27 14:48:01 +0300

    Allure_Helper ready

--- src/main/java/utils/Endpoints.java
+++ src/main/java/utils/Endpoints.java
@@ -1,8 +1,6 @@
 package utils;
 
 public class Endpoints {
-    public static final String GET_ALL_USERS = "/index.php?/api/v2/get_users";
-    public static final String GET_USERS = "/index.php?/api/v2/get_users/{id}";
     public static final String GET_PROJECTS = "index.php?/api/v2/get_projects";
     public static final String GET_PROJECT = "index.php?/api/v2/get_project/";
     public static final String ADD_PROJECT = "index.php?/api/v2/add_project";
